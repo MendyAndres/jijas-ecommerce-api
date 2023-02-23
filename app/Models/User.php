fcdda11b4a503adf882388932e7bba0e9b3b1c72
Mendy <andresmndeze@gmail.com>
2023-02-23 16:07:20 -0300

    feature #10 create address full model, create address test, address factory y migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -42,8 +44,13 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function user_type()
+    public function user_type(): BelongsTo
     {
         return $this->belongsTo(UserType::class);
+    }
+
+    public function addresses(): HasMany
+    {
+        return $this->hasMany(Address::class);
     }
 }
--- tests/Unit/Models/UserTest.php
+++ tests/Unit/Models/UserTest.php
@@ -4,6 +4,7 @@ namespace Tests\Unit\Model;
 
 use App\Models\User;
 use App\Models\UserType;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -12,10 +13,17 @@ class UserTest extends TestCase
     
     use RefreshDatabase;
 
-    public function testBelongsToUserType()
+    public function testBelongsToUserType(): void
     {
         $user = User::factory()->create();
 
         $this->assertInstanceOf(UserType::class, $user->user_type);
+    }
+
+    public function testHasManyAddresses(): void
+    {
+        $user = new User();
+
+        $this->assertInstanceOf(Collection::class, $user->addresses);
     }
 }
