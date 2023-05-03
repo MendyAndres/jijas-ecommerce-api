2fee033e0af9863792c1f63d85de3e628971b6ab
Andres Mendez <jmendez@afluenta.com>
2023-05-02 23:13:18 -0300

    feature - #32 create category logic

---
+++ app/Services/CategoryService.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Services;
+
+use App\Http\Requests\Category\StoreRequest;
+use App\Http\Requests\category\UpdateRequest;
+use App\Models\Category;
+
+class CategoryService
+{
+    public function index(): array
+    {
+        return Category::all()->toArray();
+    }
+
+    public function store(StoreRequest $request): array
+    {
+        $category = Category::create($request->all());
+        return $category->toArray();
+    }
+
+    public function show(string $categoryId): array
+    {
+        $category = Category::findOrFail($categoryId);
+        return $category->toArray();
+    }
+
+    public function update(UpdateRequest $request, string $categoryId): array
+    {
+        $category = Category::findOrFail($categoryId);
+        $category->fill($request->except('id'));
+
+        if($category->isClean()) {
+            return ['error' => 'At least one value must change', 'code' => 422];
+        }
+
+        $category->save();
+
+        return $category->toArray();
+    }
+
+    public function destroy(string $id): array
+    {
+        $category = Category::findOrFail($id);
+        $category->delete();
+        return $category->toArray();
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -30,3 +31,4 @@ Route::middleware('auth:api')->group(
 );
 
 Route::resource('products', ProductController::class);
+Route::resource('categories', CategoryController::class);
